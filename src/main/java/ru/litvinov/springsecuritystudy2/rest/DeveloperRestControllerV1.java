1886c27501151390f00629af88fa1a87e3f5f209
Litvinov Dmitriy <dilitand@gmail.com>
2020-12-13 19:38:00 +0300

    update

--- src/main/java/ru/litvinov/springsecuritystudy2/rest/DeveloperRestControllerV1.java
+++ src/main/java/ru/litvinov/springsecuritystudy2/rest/DeveloperRestControllerV1.java
@@ -1,9 +1,7 @@
 package ru.litvinov.springsecuritystudy2.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import ru.litvinov.springsecuritystudy2.model.Developer;
 
 import javax.swing.event.DocumentEvent;
@@ -27,8 +25,22 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('developers:read')")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id)).findFirst().orElse(null);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAnyAuthority('developers:write')")
+    public Developer create(@RequestBody Developer developer){
+        this.DEVELOPERS.add(developer);
+        return developer;
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('developers:write')")
+    public void deleteById(@PathVariable Long id){
+        this.DEVELOPERS.removeIf(x -> x.getId().equals(id));
     }
 
 }
